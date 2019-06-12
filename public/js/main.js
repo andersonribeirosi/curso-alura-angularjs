ec6445e7d25f80820d9859022226386a67939647
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2019-06-11 23:12:58 -0300

    Utilizando rotas

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,17 @@
 angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
-.config(function($routeProvider){
-    $routeProvider.when('/fotos', {
-        templateUrl: 'partials/principal.html',
-        controller: 'FotosController'
-    });
+    .config(function ($routeProvider, $locationProvider) {
 
-});
+        $locationProvider.html5Mode(true);
+
+        $routeProvider.when('/fotos', {
+            templateUrl: 'partials/principal.html',
+            controller: 'FotosController'
+        });
+
+        $routeProvider.when('/fotos/new', {
+            templateUrl: 'partials/foto.html'
+        });
+
+        $routeProvider.otherwise({ redirectTo: '/fotos' });
+
+    });
